497bd4bf38cf15c4b48397c81f19078626fdc9c5
ntwari-egide <ntwariegide2@gmail.com>
2022-02-27 08:25:44 +0200

    ft(charts): implement chart

--- src/views/WelcomeDashboard.jsx
+++ src/views/WelcomeDashboard.jsx
@@ -8,10 +8,35 @@ import { DashboardLayout } from "../models/DashboardLayout"
 import {AiOutlineUserAdd} from 'react-icons/ai'
 import {IoMdAdd} from 'react-icons/io'
 import {HiOutlineCubeTransparent} from 'react-icons/hi'
+import { Pie } from "@nivo/pie";
+import { ThemeProvider, SvgWrapper } from "@nivo/core";
+import { BoxLegendSvg } from "@nivo/legends";
 
 const {Title} = Typography
 
 export const WelcomeDashboard = () => {
+
+    const data = [
+        {
+          id: "rented",
+          label: "Rented",
+          value: 210,
+          color: "#FEB211"
+        },
+        {
+          id: "lost",
+          label: "Lost",
+          value: 175,
+          color: "#F1F3F2"
+        },
+        {
+          id: "available properties",
+          label: "javascript",
+          value: 410,
+          color: "#0597F2"
+        }
+      ];
+
     return <DashboardLayout defaultSelectedKeys={1}>
     
     <Title className=" text-xl mt-4 font-medium">Dashboard</Title>
@@ -41,10 +66,75 @@ export const WelcomeDashboard = () => {
         </div>
 
         <Space direction="vertical" className="pl-8 ml-4 border-l-[1px] border-neutral-800 h-[82vh]">
-            <Button className=" rounded-md w-[16em] border-none background_blue text-white h-12 hover:scale-[1.03]">Add new property</Button>
+            <Button className=" rounded-md w-[18em] border-none background_blue text-white h-12 hover:scale-[1.03]">Add new property</Button>
 
             <Space direction="vertical" className="bg-white rounded-md w-full h-96 p-6">
-dd
+            <Pie
+                    width={200}
+                    height={200}
+                    data={data}
+                    margin={{
+                        top: 20,
+                        right: 20,
+                        bottom: 20,
+                        left: 20
+                    }}
+                    enableArcLabels={false}
+                    enableArcLinkLabels={false}
+                    colors={['#0597F2','#FEB211','#F1F3F2']}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    colorBy="id"
+                    borderColor="inherit:darker(0.6)"
+                    radialLabelsSkipAngle={10}
+                    radialLabelsTextXOffset={6}
+                    radialLabelsTextColor="#333333"
+                    radialLabelsLinkOffset={0}
+                    radialLabelsLinkDiagonalLength={16}
+                    radialLabelsLinkHorizontalLength={24}
+                    radialLabelsLinkStrokeWidth={1}
+                    radialLabelsLinkColor="inherit"
+                    slicesLabelsSkipAngle={10}
+                    slicesLabelsTextColor="#333333"
+                    animate={true}
+                    motionStiffness={90}
+                    motionDamping={15}
+                    theme={{
+                        tooltip: {
+                        container: {
+                            fontSize: "13px"
+                        }
+                        },
+                        labels: {
+                        text: { color: "#555" }
+                        }
+                    }}
+                    />
+
+                    <ThemeProvider>
+                        <SvgWrapper
+                            height={100}
+                            width={200}
+                            margin={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                        >
+                            <BoxLegendSvg
+                            anchor="center"
+                            data={data}
+                            containerWidth={400}
+                            containerHeight={100}
+                            height={100}
+                            width={200}
+                            direction="row"
+                            itemWidth={90}
+                            itemHeight={20}
+                            itemsSpacing={15}
+                            padding={10}
+                            symbolSize={12}
+                            symbolShape="square"
+                            />
+                        </SvgWrapper>
+                        </ThemeProvider>
             </Space>
         </Space>
     </div>
