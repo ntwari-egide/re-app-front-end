b6d4c0061b8199c1c0b4723ac25ef8f1e902eed5
ntwari-egide <ntwariegide2@gmail.com>
2022-02-28 04:49:32 +0200

    ft(border): update border color

--- src/views/WelcomeDashboard.jsx
+++ src/views/WelcomeDashboard.jsx
@@ -3,7 +3,7 @@
  * @description: welcome dashboard component
  */
 
-import { Button, Space, Typography } from "antd"
+import { Button, Col, Divider, Row, Space, Typography } from "antd"
 import { DashboardLayout } from "../models/DashboardLayout"
 import {AiOutlineUserAdd} from 'react-icons/ai'
 import {IoMdAdd} from 'react-icons/io'
@@ -41,34 +41,34 @@ export const WelcomeDashboard = () => {
     
     <Title className=" text-xl mt-4 font-medium">Dashboard</Title>
 
-    <div className="w-[70vw] flex flex-row" isMenuCollapsed={false}>
-        <div className="w-[100%] ">
-            <Space size={19} direction="horizontal" className="mt-6 w-full">
-                <div className="pt-6 p-4 background_yellow w-[19vw] rounded-md cursor-pointer hover:scale-[1.03]">
+    <Row className="flex flex-row" isMenuCollapsed={false}>
+        <Col span={18}>
+            <Row size={19} direction="horizontal" className="mt-6 w-full">
+                <Col span={7} className="pt-6 p-4 background_yellow rounded-md cursor-pointer hover:scale-[1.03]">
                     <AiOutlineUserAdd fontSize={20} color="white" className="ml-[45%]" />
                     <p className="text-center text-white mt-4">Add new user</p>
-                </div>
+                </Col>
 
-                <div className="pt-6 p-4 bg-white w-[19vw] rounded-md cursor-pointer hover:scale-[1.03]">
+                <Col span={7}  className="pt-6 p-4 ml-12 w-fit bg-white rounded-md cursor-pointer hover:scale-[1.03]">
                     <IoMdAdd fontSize={20}   className="ml-[45%]" />
                     <p className="text-center text-black mt-4">Add new property</p>
-                </div>
+                </Col>
 
-                <div className="pt-6 p-4 bg-white w-[19vw] rounded-md cursor-pointer hover:scale-[1.03]">
+                <Col span={7}  className="pt-6 ml-12 p-4 bg-white rounded-md cursor-pointer hover:scale-[1.03]">
                     <HiOutlineCubeTransparent fontSize={20} color="black" className="ml-[45%]" />
                     <p className="text-center text-black mt-4">Rent a property</p>
-                </div>
-            </Space>
+                </Col>
+            </Row>
 
-            <div className="w-full bg-white p-1 mt-8 h-80 rounded-md">
+            <div className="w-[98%] bg-white p-1 mt-8 h-80 rounded-md">
 
             </div>
-        </div>
+        </Col>
 
-        <Space direction="vertical" className="pl-8 ml-4 border-l-[1px] border-neutral-800 h-[82vh]">
-            <Button className=" rounded-md w-[18em] border-none background_blue text-white h-12 hover:scale-[1.03]">Add new property</Button>
+        <Col span={6} className="pl-8 border-l-[1px] border-neutral-300 h-[82vh]">
+            <Button className=" rounded-md w-full border-none background_blue text-white h-12 hover:scale-[1.03]">Add new property</Button>
 
-            <Space direction="vertical" className="bg-white rounded-md w-full h-96 p-6">
+            <Space direction="vertical" className="bg-white rounded-md mt-8 w-full h-96 p-6">
             <Pie
                     width={200}
                     height={200}
@@ -136,7 +136,7 @@ export const WelcomeDashboard = () => {
                         </SvgWrapper>
                         </ThemeProvider>
             </Space>
-        </Space>
-    </div>
+        </Col>
+    </Row>
 </DashboardLayout>
 }
